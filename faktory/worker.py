e93d46f3ef5cb5414940393319bdd4e651a9a316
Tyler Swayne <tswayne@uber.com>
2020-06-11 12:50:51 -0700

    undo - faktory was just rendering full trace

--- faktory/worker.py
+++ faktory/worker.py
@@ -3,7 +3,6 @@ import signal
 import sys
 import time
 import uuid
-import traceback
 from collections import namedtuple
 from concurrent.futures import Executor, ProcessPoolExecutor, ThreadPoolExecutor
 from concurrent.futures.process import BrokenProcessPool
@@ -275,7 +274,7 @@ class Worker:
         response = {"jid": jid}
         if exception is not None:
             response["errtype"] = type(exception).__name__
-            response["message"] = traceback.format_exc()
+            response["message"] = str(exception)
 
         self.faktory.reply("FAIL", response)
         ok = next(self.faktory.get_message())
